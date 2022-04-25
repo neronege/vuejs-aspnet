a66e6f49ef6977d82de63737973f2ded8ccc2870
Egemen Ozmen <72514229+neronege@users.noreply.github.com>
2022-04-25 15:15:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var app = new Vue({
     el: '#app',
+    created(){
+        this.GetCategories()
+        
+    },
     data: {
       message: 'Merhaba',
-      created(){
-          this.GetCategories();
-      },
-      categories:[]
+     
+      categories:[],
+      category:{},
+      UserName:'',
+      password:''
     },
     methods:{
         GetCategories: function(){
@@ -14,15 +19,67 @@ var app = new Vue({
             .then(response =>{
                
                 if(response.data.isSuccessFull==true){
-                    console.log(response)
+                   
                 this.categories = response.data.entities;
                  }
+                 console.log(this.categories)
+              
                  }
-            
+                
             )
             .catch(err=>{
              console.log(err.status.text)
             })
-        }
+        },
+        GeetCategoryById: function(category){
+            axios.get('http://localhost:61307/api/category/'+ category.id)
+            .then(response =>{
+               
+                if(response.data.isSuccessFull==true){
+                   
+                this.category = response.data.entity;
+                 }
+                 console.log(this.category)
+                 console.log(data)
+                 }
+                
+            )
+            .catch(err=>{
+             console.log(err.status.text)
+            })
+        },
+        login : function(UserName, password){
+            
+            var dat = {
+                'UserName': UserName,
+                'password': password
+            }
+
+            var headers = {
+                withCredentiasl : true,
+                headers: {'Content-Type': 'application/json-patch'+'json'}
+            }
+            return axios.post('https://localhost:44393/Autt', dat, headers)
+            .then(response => {
+                if(response.data.token){
+                   localStorage.setItem('user',JSON.stringify(response));
+                }
+                return response;
+            })
+            .catch(err =>{
+                return err;
+            })
+        },
+
+    loginOnSubmit : function(){
+        this.login(this.UserName, this.password)
+        .then(response => {
+            if(response.status ==200){
+                alert("Giriş başarılı");
+            }else{
+                alert("Başarısız işlem");
+            }
+        })
+    } 
     }
-  })
+})
